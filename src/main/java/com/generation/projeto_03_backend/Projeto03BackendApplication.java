b5da7d195c4c93bdd1c30238678eee3560d17997
Thiago Tasseli <thiagotasseli@outlook.com.br>
2025-06-03 10:03:47 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/generation/projeto_03_backend/Projeto03BackendApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_03_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto03BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto03BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/projeto_03_backend/Projeto03BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projeto_03_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto03BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
